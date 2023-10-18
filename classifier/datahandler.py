86c45bf30c4a51f30279c72af0545ebe234a640b
KoT3sts <85979064+KoT3sts@users.noreply.github.com>
2023-10-17 21:00:34 -0400

    datahandler.py

--- classifier/datahandler.py
+++ classifier/datahandler.py
@@ -1,10 +1,29 @@
-import tensorflow as tf
+from numpy import mean
+from numpy import std
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
 
-class DataHandler():
-    def get_data():
-        pass
+def load_dataset():
+    # load dataset
+    (trainX, trainY), (testX, testY) = mnist.load_data()
+    
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    
+    return trainX, trainY, testX, testY
 
-class MINSTDataHandler(DataHandler):
-    def get_data():
-        mnist = tf.keras.datasets.mnist
-        return mnist.load_data()
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    
+    return train_norm, test_norm
