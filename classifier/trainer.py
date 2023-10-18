7e5a059fa7cd2c52fcc79c96f7d116329a8e3199
KoT3sts <85979064+KoT3sts@users.noreply.github.com>
2023-10-17 21:01:37 -0400

    Update trainer.py

--- classifier/trainer.py
+++ classifier/trainer.py
@@ -1,24 +1,44 @@
-from classifier import model, datahandler
-import tensorflow as tf 
-class Trainer():
-    def __init__(self):
-        self.data = datahandler.MINSTDataHandler().get_data()
+def summarize_diagnostics(histories):
+    for i in range(len(histories)):
+        # plot loss
+        plt.subplot(2, 1, 1)
+        plt.title('Cross Entropy Loss')
+        plt.plot(histories[i].history['loss'], color='blue', label='train')
+        plt.plot(histories[i].history['val_loss'], color='orange', label='test')
+        
+        # plot accuracy
+        plt.subplot(2, 1, 2)
+        plt.title('Classification Accuracy')
+        plt.plot(histories[i].history['accuracy'], color='blue', label='train')
+        plt.plot(histories[i].history['val_accuracy'], color='orange', label='test')
+        
+        plt.show()
 
-    def batch_train(self, low, high):
-        mod = model.Model()
+def summarize_performance(scores):
+    # print summary
+    print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
+    
+    # box and whisker plots of results
+    plt.boxplot(scores)
+    plt.show()
 
+def run_test_harness():
+    # load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    
+    # prepare pixel data
+    trainX, testX = prep_pixels(trainX, testX)
+    
+    # evaluate model
+    scores, histories = evaluate_model(trainX, trainY)
+    
+    # learning curves
+    summarize_diagnostics(histories)
+    
+    # summarize estimated performance
+    summarize_performance(scores)
 
-
-    def __add_layers(self, num_layers, callback, **kwargs):
-        for _ in range(0, num_layers):
-            callback(**kwargs)
-
-
-    def __conv_layer(self, filters, kernel_size, activation, pool_size):
-        return tf.keras.layers.Conv2D(filters,kernel_size, activation), tf.keras.layers.MaxPool2D(pool_size) 
-    def __flatten(self):
-        return tf.keras.layers.Flatten()
-    def __dense_layer(self, units, activation, dropout):
-        return tf.keras.layers.Dense(units, activation), tf.keras.layers.Dropout(dropout)
+# entry point, run the test harness
+run_test_harness()
 
 
